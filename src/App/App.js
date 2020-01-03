255687b0cb1b46a30e7e1cf544767323ca5fd116
Maria <mariabrock414@gmail.com>
2020-01-03 12:31:28 -0600

    made final setup edits

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import '../styles/index.scss';
+import '../styles/_variables.scss';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-danger">Bootstrap Button</button>
+      <button className="btn btn-danger">Book Tracker Button</button>
     </div>
   );
 }
