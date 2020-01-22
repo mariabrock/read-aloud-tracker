9b5990a25b41633160fde32bb0a58e1bde995682
Maria <mariabrock414@gmail.com>
2020-01-21 21:25:59 -0600

    auth with facebook wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import firebase from 'firebase';
+
 import './App.scss';
 import '../styles/index.scss';
 import '../styles/_variables.scss';
 
-function App() {
+class App extends React.Component {
+state = {
+  authed: false,
+}
+
+componentDidMount() {
+  this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ authed: true });
+    } else {
+      this.setState({ authed: false });
+    }
+  });
+}
+
+componentWillUnmount() {
+  this.removeListener();
+}
+
+render() {
+  // const { authed } = this.state;
   return (
-    <div className="App">
-      <button className="btn btn-danger">Book Tracker Button</button>
-    </div>
+      <div className="App">
+        <button className="btn btn-danger">Book Tracker Button</button>
+      </div>
   );
+}
 }
 
 export default App;
--- src/components/pages/Auth/Auth.js
+++ src/components/pages/Auth/Auth.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import firebase from 'firebase';
+import 'firebase/auth';
+
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.FacebookAuthProvider();
+    provider.addScope('user_birthday');
+    firebase.auth().signInWithPopup(provider).then((result) {
+      // This gives you a Facebook Access Token.
+      const token = result.credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+    });
+  }
+
   // render() {
-  //   return ();
+  //   return (
+  //   <div className="Auth">
+  //       <h1>Auth Page</h1>
+  //       <button className="btn btn-danger" onClick={this.loginClickEvent}>Login With Google</button>
+  //     </div>
+  //   );
   // }
-}
 
 export default Auth;
