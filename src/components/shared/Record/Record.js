c8c6e1984e658bb52b9e1fcc180c4b8c056ae8cd
Maria <mariabrock414@gmail.com>
2020-01-28 19:36:20 -0600

    added moment to the record cards

--- src/components/shared/Record/Record.js
+++ src/components/shared/Record/Record.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import './Record.scss';
 
 import recordShape from '../../../helpers/propz/recordShape';
@@ -24,7 +25,7 @@ class Record extends React.Component {
             <div className="card-body">
               <p className="card-text">Time: {record.duration}</p>
               <p className="card-text">Pages: {record.numOfPagesRead}</p>
-              <p className="card-text"><small className="text-muted">Date: Last updated 3 mins ago</small></p>
+              <p className="card-text"><small className="text-muted"><p>{moment(record.date).format('MMMM Do YYYY, h:mm:ss a')}</p></small></p>
               <button className ="del-record btn btn-danger" onClick={this.deleteRecordEvent}>X</button>
             </div>
         </div>
