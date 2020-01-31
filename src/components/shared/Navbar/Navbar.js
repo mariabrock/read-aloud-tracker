7c241a876f4cec1eef62a51fbe20f9e41d5b2a75
Maria <mariabrock414@gmail.com>
2020-01-30 20:58:19 -0600

    some styling and fixed date

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -29,20 +29,11 @@ class Navbar extends React.Component {
         if (authed) {
           return (
             <ul className="navbar-nav ml-auto">
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/booklist"><FontAwesomeIcon icon={faHome}/></Link>
-              </li> */}
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/book/:bookId"><FontAwesomeIcon icon={faBookmark}/></Link> */}
-              {/* </li> */}
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/record"><FontAwesomeIcon icon={faClock}/></Link>
-              </li> */}
               <li className="nav-item">
-                <Link className="nav-link" to="/"><FontAwesomeIcon icon={faList} size="lg" /></Link>
+                <Link className="nav-link" to="/"><FontAwesomeIcon icon={faList} id="list" size="2x" /></Link>
               </li>
               <li className="nav-item">
-                <button className="nav-link btn btn-primary" onClick={this.logMeOut}>Logout</button>
+                <button className="nav-link btn btn-primary d-none d-lg-inline-block" onClick={this.logMeOut}>Logout</button>
               </li>
             </ul>
           );
