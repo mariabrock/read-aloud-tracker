c28fe9a1a7ceb59a775450747f2e6836e8d73b2e
Maria <mariabrock414@gmail.com>
2020-02-01 13:05:00 -0600

    altered styling

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -43,7 +43,7 @@ class Navbar extends React.Component {
 
       return (
         <div className="MyNavbar">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
               <Link className="navbar-brand" to="/">Imagine Out Loud</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
